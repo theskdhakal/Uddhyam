d5d062e3e338fbfdd44c435d3043c4d4c4223712
Shiva Dhakal <theskdhakal@gmail.com>
2024-06-25 14:52:10 +1000

    native wind installed

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-center text-3xl">Home</Text>
+      <Link href="/profile">Go to Profile</Link>
     </View>
   );
 };
+
+export default Home;
