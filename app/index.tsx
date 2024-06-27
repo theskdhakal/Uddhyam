4350f60f1c787ed287b2eb2328185be056cbf679
Shiva Dhakal <theskdhakal@gmail.com>
2024-06-27 22:23:53 +1000

    entry page connected to signIn

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,15 @@
 import { Image, ScrollView, Text, View } from "react-native";
-
+import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "@/constants";
+import CustomButton from "@/components/CustomButton";
+import { router } from "expo-router";
 
 const Home = () => {
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView contentContainerStyle={{ height: "100%" }}>
-        <View className="w-full justify-center items-center h-full px-4">
+        <View className="w-full justify-center items-center min-h-[85vh] px-4">
           <Image
             source={images.logo}
             className="w-[130px] h-[84px]"
@@ -35,8 +37,14 @@ const Home = () => {
           <Text className="text-sm font-pregular text-gray-100 mt-7 text-center ">
             Where efforts meet the dream:start journey of your dream career
           </Text>
+          <CustomButton
+            title="Continue with Email"
+            handlePress={() => router.push("/sign-in")}
+            containerStyles="w-full mt-7"
+          />
         </View>
       </ScrollView>
+      <StatusBar backgroundColor="#051029" style="light" />
     </SafeAreaView>
   );
 };
