d356e1ec00f1baf28942a37f3cb41b9aceeacdd4
Shiva Dhakal <theskdhakal@gmail.com>
2024-06-27 20:09:20 +1000

    working in main page

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,43 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
+
+import { SafeAreaView } from "react-native-safe-area-context";
+import { images } from "@/constants";
 
 const Home = () => {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-center text-3xl font-pblack">Khoj</Text>
-      <Link href="/home">Go to Home</Link>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="w-full justify-center items-center h-full px-4">
+          <Image
+            source={images.logo}
+            className="w-[130px] h-[84px]"
+            resizeMode="contain"
+          />
+          <Image
+            source={images.cards}
+            className="mX-w-[380px] W-FULL h-[300px]"
+            resizeMode="contain"
+          />
+
+          <View className="relative mt-4">
+            <Text className="text-3xl text-white font-bold text-center">
+              Dicover Endless Possibilities with{" "}
+              <Text className="text-secondary-200">Uddhyam</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className="w-[136px] h-[115px] absolute top-14 right-14  "
+              resizeMode="contain"
+            />
+          </View>
+
+          <Text className="text-sm font-pregular text-gray-100 mt-7 text-center ">
+            Where efforts meet the dream:start journey of your dream career
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
