7aa739e9c2b62e1c0e963c507b0351d217763a5a
Shiva Dhakal <theskdhakal@gmail.com>
2024-07-02 15:21:41 +1000

    some backend config added

---
+++ lib/appwrite.ts
@@ -0,0 +1,8 @@
+export const appwriteConfig = {
+  endpoint: "https://cloud.appwrite.io/v1",
+  platform: "com.jsm.uddhyam",
+  projectId: "6683654a001500d3c309",
+  databaseId: "66838cea0012cf6c9e93",
+  userCollectionId: "66838d0100181e9def4e",
+  videosCollectionId: "66838d350007992d3259",
+};
