92e45c2a94621b556dd910a0b818fc7443a75577
Shiva Dhakal <theskdhakal@gmail.com>
2024-07-16 12:43:43 +1000

    submit function made functional

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,8 +1,71 @@
-export const appwriteConfig = {
+import { Client, Account, ID, Avatars, Databases } from "react-native-appwrite";
+
+interface userProps {
+  email: string;
+  password: string;
+  username?: string;
+}
+
+export const config = {
   endpoint: "https://cloud.appwrite.io/v1",
   platform: "com.jsm.uddhyam",
   projectId: "6683654a001500d3c309",
   databaseId: "66838cea0012cf6c9e93",
   userCollectionId: "66838d0100181e9def4e",
   videosCollectionId: "66838d350007992d3259",
+  storageId: "6685def400398192037b",
+};
+
+// Init your React Native SDK
+const client = new Client();
+
+client
+  .setEndpoint(config.endpoint)
+  .setProject(config.projectId)
+  .setPlatform(config.platform);
+
+const account = new Account(client);
+const avatars = new Avatars(client);
+const databases = new Databases(client);
+
+export const createUser = async ({ email, password, username }: userProps) => {
+  try {
+    const newAccount = await account.create(
+      ID.unique(),
+      email,
+      password,
+      username
+    );
+
+    if (!newAccount) throw Error;
+
+    const avatarUrl = avatars.getInitials(username);
+
+    await signIn({ email, password });
+
+    const newUser = await databases.createDocument(
+      config.databaseId,
+      config.userCollectionId,
+      ID.unique(),
+      {
+        accountId: newAccount.$id,
+        email,
+        username,
+        avatar: avatarUrl,
+      }
+    );
+
+    return newUser;
+  } catch (error: any) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
+
+export const signIn = async ({ email, password }: userProps) => {
+  try {
+    const sesseion = await account.createEmailPasswordSession(email, password);
+  } catch (error: any) {
+    throw new Error(error);
+  }
 };
