4f983d901379c354abfae1a57b3916a8547c4056
Shiva Dhakal <theskdhakal@gmail.com>
2024-07-22 22:32:57 +1000

    video animation added to trending section

--- components/Trending.tsx
+++ components/Trending.tsx
@@ -1,13 +1,67 @@
-import { View, Text, FlatList } from "react-native";
-import React from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  TouchableOpacity,
+  ImageBackground,
+  ViewStyle,
+} from "react-native";
+import React, { useState } from "react";
+import * as Animatable from "react-native-animatable";
+
+const zoomIn: any = {
+  0: {
+    scale: 0.9,
+  },
+  1: {
+    scale: 1,
+  },
+};
+
+const zoomOut: any = {
+  0: {
+    scale: 1,
+  },
+  1: {
+    scale: 0.9,
+  },
+};
+
+const TrendingItem = ({ activeItem, item }) => {
+  const [play, setPlay] = useState(false);
+
+  const animation = activeItem === item.$id ? zoomIn : zoomOut;
+
+  return (
+    <Animatable.View className="mr-5" animation={animation} duration={500}>
+      {play ? (
+        <Text className="text-white">Playinh</Text>
+      ) : (
+        <TouchableOpacity
+          className="relative justify-center items-center "
+          activeOpacity={0.7}
+          onPress={() => setPlay(true)}
+        >
+          <ImageBackground
+            source={{ uri: item.thumbnail }}
+            className="w-52 h-72 rounded-[35px] my-5 overflow-hidden shadow-lg shadow-black-40"
+          />
+        </TouchableOpacity>
+      )}
+    </Animatable.View>
+  );
+};
 
 const Trending = ({ posts }) => {
+  const [activeItem, setActiveItem] = useState(posts[0]);
+
   return (
     <FlatList
       data={posts}
       keyExtractor={(item) => item.$id}
       renderItem={({ item }) => (
-        <Text className="text-3xl text-white">{item.id}</Text>
+        <TrendingItem activeItem={activeItem} item={item} />
       )}
       horizontal
     />
